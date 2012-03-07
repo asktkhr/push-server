672fd8535a958ada719d76d35d22fd72fbfaa239
asktkhr <bbclapton@gmail.com>
2012-03-08 00:50:55 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushServer::Application.config.secret_token = '9dc3d50b2c8bd03c0f155cbf2da0f01ea76a678b686c407b8aba05705de4f19b135e503190bcf11d619fbd09e5f085a07532112e90bcdf910099f6b82afe115c'
