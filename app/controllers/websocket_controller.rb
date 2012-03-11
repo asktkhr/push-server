b386e78e3b791b9e37f58a2bf1080a21357f36e8
asktkhr <bbclapton@gmail.com>
2012-03-11 21:57:30 +0900

    request pusher to push url

--- app/controllers/websocket_controller.rb
+++ app/controllers/websocket_controller.rb
@@ -1,4 +1,6 @@
 class WebsocketController < ApplicationController
   def send_url
+    Message.new.send_url params
+    render :nothing => true, :status => '200'
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,8 @@
 class Message
   require 'pusher'
 
-  def send_url url
-    Pusher['channel_name'].trigger('event_name', url)
-
+  def send_url params
+    Pusher['channel_name'].trigger('event_name', params[:url], params[:socket_id])
   end
 
 end
