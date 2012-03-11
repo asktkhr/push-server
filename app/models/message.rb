ed542fab4dd528215c5d703f2beb6be8c3a07b40
asktkhr <bbclapton@gmail.com>
2012-03-11 16:53:08 +0900

    move initialization process

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,6 @@
 class Message
   require 'pusher'
 
-  Pusher.app_id = 'app_id'
-  Pusher.key = 'app_key'
-  Pusher.secret = 'app_secret'
-
   def send_url url
     Pusher['channel_name'].trigger('event_name', url)
 
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,3 @@
+Pusher.app_id = 'app_id'
+Pusher.key = 'app_key'
+Pusher.secret = 'app_secret'
