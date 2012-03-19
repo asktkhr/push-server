bc1e568ffe23d277a62404c144b7aae348b36704
asktkhr <bbclapton@gmail.com>
2012-03-19 17:02:02 +0900

    change to be able to specify the recipient

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,12 @@ class Message
   require 'pusher'
 
   def send_url params
+    data = {}
+    data[:url] = params[:url]
+    data[:receivers] = params[:receivers]
+
     begin
-      Pusher['chrome2chrome'].trigger('open_url', params[:url], params[:socket_id])
+      Pusher['chrome2chrome'].trigger('open_url', data.to_json, params[:socket_id])
     rescue Pusher::Error => e
       p e
     end
