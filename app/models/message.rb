8b21c32f43a5badadd8d1a7215ad31b1a61bc510
asktkhr <bbclapton@gmail.com>
2012-03-19 12:45:09 +0900

    add handle errors by rescuing

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,10 @@ class Message
   require 'pusher'
 
   def send_url params
-    Pusher['chrome2chrome'].trigger('open_url', params[:url], params[:socket_id])
+    begin
+      Pusher['chrome2chrome'].trigger('open_url', params[:url], params[:socket_id])
+    rescue Pusher::Error => e
+      p e
+    end
   end
-
 end
